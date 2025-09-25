824452a402ac5caf2b303bf6c3a5946ed4120149
DjilaniB <djilanibourhan@gmail.com>
2025-09-25 07:53:51 +0200

    counterMinus FINALLY in

--- index.js
+++ index.js
@@ -1,59 +1,37 @@
-//
+const counterDisplay = document.querySelector("h3");
+let counter = 0;
 
-// const counterDisplay = document.querySelector("h3");
-// const body = document.querySelector(".allMight");
-// let counter = 0;
+const bubbleMaker = () => {
+  const bubble = document.createElement("span");
+  bubble.classList.add("bubble");
+  document.body.appendChild(bubble);
 
-// function updateScore() {
-//   counterDisplay.textContent = counter;
-//   console.log(counter);
-// }
+  const size = Math.random() * 200 + 100 + "px";
+  bubble.style.height = size;
+  bubble.style.width = size;
 
-// const bubbleMaker = () => {
-//   const bubble = document.createElement("span");
-//   bubble.classList.add("bubble");
-//   document.body.appendChild(bubble);
+  bubble.style.top = Math.random() * 100 + 50 + "%";
+  bubble.style.left = Math.random() * 100 + "%";
 
-//   const size = Math.random() * 200 + 100 + "px";
-//   bubble.style.height = size;
-//   bubble.style.width = size;
+  const plusMinus = Math.random() > 0.5 ? 1 : -1;
+  bubble.style.setProperty("--left", Math.random() * 100 * plusMinus + "%");
 
-//   bubble.style.top = Math.random() * 100 + 50 + "%";
-//   bubble.style.left = Math.random() * 100 + "%";
+  bubble.addEventListener("click", () => {
+    counter++;
+    counterDisplay.textContent = counter;
+    bubble.remove();
+  });
 
-//   const plusMinus = Math.random() > 0.5 ? 1 : -1;
+  setTimeout(() => {
+    bubble.remove();
+  }, 8000);
+};
 
-//   bubble.style.setProperty("--left", Math.random() * 100 * plusMinus + "%");
+setInterval(bubbleMaker, 1000);
 
-//   bubble.addEventListener("click", (event) => {
-//     console.log(e);
-//     counter++;
-//     updateScore();
-//     bubble.remove();
-//   });
-
-//   document.body.addEventListener("click", (e) => {
-//     console.log(e);
-//     counter--;
-//     updateScore();
-//     bubble.remove();
-//   });
-
-//   setTimeout(() => {
-//     bubble.remove();
-//   }, 8000);
-
-//   updateScore();
-// };
-
-// setInterval(bubbleMaker, 300);
-
-// updateScore();
-
-//   bubble.addEventListener("click", () => {
-//   counter++;
-//   counterDisplay.textContent = counter;
-//   bubble.remove();
-//   console.log("counter =" + counter);
-//   console.log("minus =" + minus);
-// });
+window.addEventListener("click", (e) => {
+  if (!e.target.classList.contains("bubble")) {
+    counter--;
+    counterDisplay.textContent = counter;
+  }
+});
